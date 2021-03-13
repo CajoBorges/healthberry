d0b198d315865249fb8f2c8e55b27182633186fa
Cajo Borges <cjvborges@gmail.com>
2021-03-13 10:03:35 +0000

    Adicionada Base de dados para utilizadores

--- app.py
+++ app.py
@@ -2,15 +2,27 @@ from flask import Flask, render_template, session, redirect, request, url_for, l
 
 from models import User
 
+import re
+import sqlite3
+
+#conn = sqlite3.connect('healthberry.db')
+#c = conn.cursor()
+#c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+
 app = Flask(__name__)
 app.config.from_pyfile("settings.py")
 
-users = {
-    "cjvborges@gmail.com":
-    User(name="Carlos", email="cjvborges@gmail.com", password="12345"),
-    "nelsonmestevao@gmail.com":
-    User(name="Nelson", email="nelsonmestevao@gmail.com", password="12345")
-}
+
+@app.errorhandler(404)
+def erro(error):
+    return render_template('pages/error.html'), 404
+
+
+@app.errorhandler(400)
+def error(error):
+    return render_template('pages/error.html'), 400
+    # TODO: replace number
 
 
 @app.route("/logout", methods=['GET'])
@@ -29,13 +41,19 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    conn = sqlite3.connect('healthberry.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM USERS')
+    users = c.fetchall()
+    for u in users:
+        print(u)
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
 
         if email in users:
-            # TODO: create error page <https://stackoverflow.com/a/21301229/8222710>
+            # TODO: create error page <https://stackoverflow.com/a/21301229/8222710> - feito
             abort(400)
 
         # TODO: verify if password is strong
@@ -43,9 +61,10 @@ def register():
         new_user = User(name, email, password)
 
         # TODO: add email validation
-        users.update({email: new_user})
-
-        session["user"] = new_user.__dict__
+        c.execute('INSERT INTO users(name, email, password) VALUES (?,?, ?)',
+                  (name, email, password))
+        conn.commit()
+        c.close()
 
         return redirect(url_for('index'))
 
@@ -54,18 +73,21 @@ def register():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
+    conn = sqlite3.connect('healthberry.db')
+    c = conn.cursor()
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        if not email in users:
-            # TODO: create error page <https://stackoverflow.com/a/21301229/8222710>
-            abort(404)
-        user = users[email]
-        if password == user.password:
+        c.execute("SELECT * FROM users WHERE email=?", (email,))
+        # TODO: create error page <https://stackoverflow.com/a/21301229/8222710> - feito
+        user = c.fetchone()
+        c.close()
+        print(user)
+        if password == user[2]:
             session["user"] = user.__dict__
         return redirect(url_for('index'))
 
-    return render_template("pages/login.html")
+    return render_template("pages/index.html")
 
 
 if __name__ == "__main__":
